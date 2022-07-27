4f252a0a00427768db1942250ec4c9262ffd5b99
fardanfh <danfardan16@gmail.com>
2022-07-27 21:43:36 +0700

    first commit

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next){
+        return $next($request)
+            ->header( 'Access-Control-Allow-Origin', "*")
+            ->header( 'Access-Control-Allow-Methods', "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+            ->header( 'Access-Control-Allow-Credentials', true)
+            ->header( 'Access-Control-Allow-Headers', "Accept , X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+    }
+}
---
+++ database/seeds/ProvincesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProvincesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $url_province = "https://api.rajaongkir.com/starter/province?key=816aecd60666309b40d1bce5f3056b88";
+        $json_str = file_get_contents($url_province);
+        $json_obj = json_decode($json_str);
+        $provinces = [];
+        foreach($json_obj->rajaongkir->results as $province){
+        $provinces[] = [
+        'id' => $province->province_id,
+        'province' => $province->province
+        ];
+        }
+        DB::table('provinces')->insert($provinces);
+
+    }
+}
